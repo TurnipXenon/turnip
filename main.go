6d65bb6fd542f1c421ff4c58b75737b0ecd26062
turnipxenon <turnipxenon@gmail.com>
2022-12-18 15:27:55 -0700

    Serve with http template

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
 
@@ -9,10 +10,49 @@ func hello(response http.ResponseWriter, _ *http.Request) {
 	response.Write([]byte("Hello"))
 }
 
+type MainData struct {
+}
+
+func handleTemplate(response http.ResponseWriter, request *http.Request) {
+	path := request.URL.Path
+	if path != "/" {
+		// todo: not found page!
+		response.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// Reference: https://gowebexamples.com/templates/
+	tmpl := template.Must(template.ParseFiles("./internal/templates/main.html"))
+	data := MainData{}
+	err := tmpl.Execute(response, data)
+	if err != nil {
+		// todo
+		return
+	}
+}
+
+func serveSingle(pattern string, filename string, mux *http.ServeMux) {
+	// from Deleplace @ https://stackoverflow.com/a/14187941/17836168
+	mux.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		print("Tests")
+		http.ServeFile(w, r, filename)
+	})
+}
+
 func main() {
+	mux := http.NewServeMux()
+
 	// todo(turnip): document how static works
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/hello/", hello)
+	mux.HandleFunc("/api/hello", hello)
+
+	// root-based resources
+	serveSingle("/robots.txt", "./assets/robots.txt", mux)
+	// todo: favicon
+	// todo: sitemap
+
+	mux.Handle("/assets/", http.FileServer(http.Dir("./")))
+	mux.HandleFunc("/", handleTemplate)
+
 	// todo(turnip): make port dynamic
-	http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":8090", mux)
 }
