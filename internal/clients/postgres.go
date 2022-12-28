3cd46cccc51421254ac86f5e56cc8d611d250b71
TurnipXenon <turnipxenon@gmail.com>
2022-12-27 18:16:31 -0700

    Create Token table if it does not exist (#59)

--- internal/clients/postgres.go
+++ internal/clients/postgres.go
@@ -2,15 +2,32 @@ package clients
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/TurnipXenon/turnip/internal/models"
+	"github.com/TurnipXenon/turnip/internal/server/sql/migration"
+	"github.com/TurnipXenon/turnip/internal/util"
+)
+
+const (
+	ifUserTableExists = `SELECT EXISTS(
+               SELECT
+               FROM information_schema.tables
+               WHERE table_schema = 'public'
+                 AND table_name = '%s'
+           );`
 )
 
 type PostgresDb struct {
 	Pool *pgxpool.Pool
+}
+
+type GenericPostgresTable interface {
+	GetTableName() string
+	GetMigrationSequence() []migration.Migration
 }
 
 // NewPostgresDatabase remember to defer DeferredClose!!!
@@ -34,4 +51,25 @@ func NewPostgresDatabase(ctx context.Context, flags models.RunFlags) *PostgresDb
 // todo safely close
 func (p *PostgresDb) DeferredClose(ctx context.Context) {
 	p.Pool.Close()
+}
+
+func IfTableExistsQuery(tableName string) string {
+	return fmt.Sprintf(ifUserTableExists, tableName)
+}
+
+func SetupTable(ctx context.Context, d *PostgresDb, t GenericPostgresTable) {
+	// todo: refactor to be a better, the side-effect is not good
+	row := d.Pool.QueryRow(ctx, IfTableExistsQuery(t.GetTableName()))
+	var exists bool
+	err := row.Scan(&exists)
+	if err != nil {
+		util.LogDetailedError(err)
+		log.Fatalf("failed to check if table exists: %v", err)
+	}
+
+	if !exists {
+		for _, m := range t.GetMigrationSequence() {
+			m.Migrate(ctx, d.Pool)
+		}
+	}
 }
