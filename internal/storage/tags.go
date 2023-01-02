c9f057a186ac8c917d81060c3ca3fd4bdd15bd11
turnipxenon <turnipxenon@gmail.com>
2023-01-01 21:05:37 -0700

    Setup tags table and interface

---
+++ internal/storage/tags.go
@@ -0,0 +1,14 @@
+package storage
+
+import (
+	"context"
+
+	"github.com/TurnipXenon/turnip_api/rpc/turnip"
+)
+
+type Tags interface {
+	UpdateTags(ctx context.Context, content *turnip.Content) error
+	GetTagsByContent(ctx context.Context, content *turnip.Content) ([]string, error)
+	GetContentIdsByTag(ctx context.Context, tagList []string) ([]string, error)
+	DeleteTags(ctx context.Context, primaryId string) error
+}
